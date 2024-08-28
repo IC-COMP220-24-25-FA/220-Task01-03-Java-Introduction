0076bcc5b8643c2e00c11c02fb6f7001a2f94660
Toby Dragon <tdragon@ithaca.edu>
2024-08-28 08:02:47 -0400

    updated java version and added passing test

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -1,13 +1,14 @@
 package edu.ithaca.dragon.practice;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 public class FunctionPracticeTest {
-
 
     @Test
     public void largestOfThreeTest(){
